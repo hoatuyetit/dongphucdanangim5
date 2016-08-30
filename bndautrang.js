e61574b273ecb2e69a72a2f2a734ec503646d4ff
hoatuyetit <thituyetnguyen9x@gmail.com>
2016-08-30 14:20:43 +0700

    Update bndautrang.js

--- bndautrang.js
+++ bndautrang.js
@@ -117,7 +117,7 @@ jQuery(window).load(function() {
 			jQuery(divpopup).attr('id', 'myModal');
 			jQuery(divpopup).attr('class', 'linhnguyen-modal');
 			jQuery( "body" ).append(divpopup);
-			jQuery("#myModal").html("<a href='http://goo.gl/rKJWvA' target='_blank'><img src='http://faceseo.vn/bannerweb/hoc-seo-chuyen-sau.jpg' width='500px'/><br/>13 Kỹ thuật đẩy top vượt trội</a>. Học tại nhà CEO Faceseo. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			jQuery("#myModal").html("<a href='http://goo.gl/rKJWvA' target='_blank'><img src='http://www.im5.vn/image/data/right-sidebar/lien-he.png' width='500px'/><br/>Cơ sở may áo thun đồng phục tại tp HCM</a> <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			jQuery('#myModal').linhnguyen(jQuery('#myModal').data());
 		}
 });
